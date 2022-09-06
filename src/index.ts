a4dc5afca9687745371f70c8bcf1fb94b2cebef3
Ermesson Lima <ermessonlima@MacBook-Air-de-Ermesson.local>
2022-09-06 01:45:50 -0300

    feat: add swagger

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+
+const swaggerFile = require("../swagger_output.json");
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use("/users", usersRoutes);
 
